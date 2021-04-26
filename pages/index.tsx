f7d7f87f2e63b784ec2ad2c65c9dba9fdb2a2cdc
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-04-26 07:12:49 -0400

    add eslint, prettier and editorconfig

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <div>
       <Head>
@@ -12,4 +12,6 @@ export default function Home() {
       </main>
     </div>
   );
-}
+};
+
+export default Home;
