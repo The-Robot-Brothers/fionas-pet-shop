519f7e45eb0a162e831817640c3fff2eb625018a
Leandro Geovanni Cruz <leandro.programmer@hotmail.com>
2021-05-04 09:53:23 -0400

    change globalStyles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,11 +5,25 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+
+    color: ${props => props.theme.colors.text}
   }
 
-  body {
-    background: ${props => props.theme.colors.background};
-    color: ${props => props.theme.colors.text};
-    font: 400 16px Roboto, sans-serif;
+  html, body, #root {
+    max-height: 100vh;
+    max-width: 100vw;
+
+    height: 100%;
+    width: 100%;
+  }
+
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: -apple-system, system-ui, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Ubuntu, Arial, sans-serif;
+  }
+
+  html {
+    background: ${props => props.theme.colors.background}
   }
 `
